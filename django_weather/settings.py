020787b349722de686af9e38c2533437112e18a0
patelvaibhav21 <101822519+patelvaibhav21@users.noreply.github.com>
2022-12-21 21:28:07 +0530

    Update settings.py

--- django_weather/settings.py
+++ django_weather/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^2cg2+kvke$rzh$c@)_v11imqti6n6yo58#g2h314@74e#1=2c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
